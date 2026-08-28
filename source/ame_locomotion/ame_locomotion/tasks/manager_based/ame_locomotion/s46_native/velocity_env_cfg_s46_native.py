679d88e6005cfd6b24a49b231729075379a607eb
winternightsong <1052365481@qq.com>
2026-08-28 12:24:19 +0800

    fix: import terrain generator config for S46 stage2

--- source/ame_locomotion/ame_locomotion/tasks/manager_based/ame_locomotion/s46_native/velocity_env_cfg_s46_native.py
+++ source/ame_locomotion/ame_locomotion/tasks/manager_based/ame_locomotion/s46_native/velocity_env_cfg_s46_native.py
@@ -15,6 +15,7 @@ from isaaclab.managers import TerminationTermCfg as DoneTerm
 from isaaclab.scene import InteractiveSceneCfg
 from isaaclab.sensors import ContactSensorCfg, RayCasterCfg, patterns
 from isaaclab.terrains import TerrainImporterCfg
+from isaaclab.terrains.terrain_generator_cfg import TerrainGeneratorCfg
 from isaaclab.utils import configclass
 from isaaclab.utils.assets import ISAAC_NUCLEUS_DIR, ISAACLAB_NUCLEUS_DIR
 from isaaclab.utils.noise import AdditiveUniformNoiseCfg as Unoise
